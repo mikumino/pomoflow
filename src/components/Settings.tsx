d49f9606d3ba5ce198b19bc075a20df30a69ed86
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-25 15:12:08 -0800

    Style changes and ts

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,10 +2,12 @@ import { useRef, useEffect } from 'react';
 import { IoIosSettings } from "react-icons/io";
 
 const Settings = () => {
-    const settingsRef = useRef(null);
+    const settingsRef = useRef<HTMLDialogElement | null>(null);
 
     const handleOpen = () => {
-        (settingsRef.current).showModal();
+        if (settingsRef.current) {
+            settingsRef.current.showModal();
+        }
     }
 
     return (
@@ -16,7 +18,7 @@ const Settings = () => {
             <dialog id="settings" ref={settingsRef} className="modal outline-none" >
                 <div className="modal-box">
                     <h1 className="text-3xl font-bold mb-4">Settings</h1>
-                    <h2 className="text-xl font-bold mb-2">Flowtime</h2>
+                    <h2 className="font-bold mb-2">Flowtime</h2>
                     <h3>Break Time Multiplier</h3>
                     <p className="text-xs mb-2">The number multiplied against focus time to get break time (Default: 0.2)</p>
                     <input type="number" className="input input-bordered w-full mb-4" />
