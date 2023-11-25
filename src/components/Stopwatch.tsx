0e40595568f6719e16fb9c356dc52c93d42f5e24
mikumino <36429067+mikumino@users.noreply.github.com>
2023-11-24 17:39:27 -0800

    Switch state on stop

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Stopwatch() {
-    const [flowBreak, setFlowBreak] = useState('flow');
     const [time, setTime] = useState(0);
+    const [isFlow, setIsFlow] = useState(true);
     const [isOn, setIsOn] = useState(false);
 
     useEffect(() => {
@@ -21,12 +21,22 @@ export default function Stopwatch() {
         setIsOn(!isOn);
     };
 
+    const handleStop = () => {
+        setIsOn(false);
+        resetTimer();
+        setIsFlow(!isFlow);
+    };
+
     const setFlow = () => {
-        setFlowBreak('flow');
+        setIsFlow(true);
+        setIsOn(false);
+        resetTimer();
     };
 
     const setBreak = () => {
-        setFlowBreak('break');
+        setIsFlow(false);
+        setIsOn(false);
+        resetTimer();
     };
 
     const resetTimer = () => {
@@ -49,14 +59,15 @@ export default function Stopwatch() {
     return (
         <div className="flex flex-col items-center justify-center">
             <div className="flex flex-row items-center justify-center mb-6">
-                <h2 className={`btn mr-2 px-11 ${flowBreak == "flow" ? 'btn-primary' : 'btn-outline'}`} onClick={setFlow}>Flow</h2>
-                <h2 className={`btn px-11 ${flowBreak == "break" ? 'btn-primary': 'btn-outline'}`} onClick={setBreak}>Break</h2>
+                <h2 className={`btn mr-2 px-11 ${isFlow ? 'btn-primary' : 'btn-outline'}`} onClick={setFlow}>Flow</h2>
+                <h2 className={`btn px-11 ${!isFlow ? 'btn-primary': 'btn-outline'}`} onClick={setBreak}>Break</h2>
             </div>
             <div className="flex flex-row items-center justify-center mb-7">
                 <h1 className="text-6xl sm:text-8xl font-bold text-center">{msToTime(time)}</h1>
             </div>
             <div className="flex flex-row items-center justify-center">
                 <div className="btn btn-circle btn-primary mr-4" onClick={handleStartPause}><iconify-icon icon={isOn ? 'ph:pause-fill' : 'ph:play-fill'} /></div>
+                <div className="btn btn-circle btn-outline btn-primary mr-4" onClick={handleStop}><iconify-icon icon="ph:stop-fill" /></div>
                 <div className="btn btn-circle btn-outline" onClick={resetTimer}><iconify-icon icon="solar:restart-broken"></iconify-icon></div>
             </div>
         </div>
